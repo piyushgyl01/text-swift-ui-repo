cdeb5222c76e835892b732b56e2571d5ad5c9384
piyush.gyl <piyush2022ug@gmail.com>
2025-02-13 01:41:22 +0530

    Initial Commit

---
+++ hws/hwsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hwsApp.swift
+//  hws
+//
+//  Created by piyush ehe on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct hwsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hwsTests/hwsTests.swift
@@ -0,0 +1,17 @@
+//
+//  hwsTests.swift
+//  hwsTests
+//
+//  Created by piyush ehe on 13/02/25.
+//
+
+import Testing
+@testable import hws
+
+struct hwsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
