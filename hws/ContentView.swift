20c1ecc7d1bf41227c9d1e41c2b39bf94c0efb4f
piyush.gyl <piyush2022ug@gmail.com>
2025-02-13 01:59:55 +0530

    first-commit

--- hws/ContentView.swift
+++ hws/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var inputValue = 0.0
+    @State private var inputUnit = "seconds"
+    @State private var outputUnit = "minutes"
+    
+    let units = ["seconds", "minutes", "hours", "days"]
+    
+    func toSeconds(value: Double, from unit: String) -> Double {
+        switch unit {
+        case "minutes": return value * 60
+        case "hours": return value * 3600
+        case "days": return value * 86400
+        default: return value
         }
-        .padding()
+    }
+    
+    func fromSeconds(_ seconds: Double, to unit: String) -> Double {
+        switch unit {
+        case "minutes": return seconds / 60
+        case "hours": return seconds / 3600
+        case "days": return seconds / 86400
+        default:
+            return seconds
+        }
+    }
+    
+    var result: Double {
+        let seconds = toSeconds(value: inputValue, from: inputUnit)
+        let finalValue = fromSeconds(seconds, to: outputUnit)
+        return finalValue
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section("Input") {
+                    TextField("Value", value: $inputValue, format: .number)
+                    
+                    Picker("From", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0.capitalized)
+                        }
+                    }
+                }
+                
+                Section("Output") {
+                    
+                    Picker("From", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0.capitalized)
+                        }
+                    }
+                    
+                    Text("\(result.formatted()) \(outputUnit)")
+                }
+                    
+            }
+            .navigationTitle("Time Converter")
+        }
     }
 }
 
